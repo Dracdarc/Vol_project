d89d7b1ed3eec7b9991d81382a7b486a367f9228
Dracdarc <rpecheul@yahoo.fr>
2022-04-04 11:14:23 +0200

    More index

--- Vol_project/functions/asset_utility.py
+++ Vol_project/functions/asset_utility.py
@@ -36,7 +36,7 @@ def get_asset_data(name: str, year: int, past_period: int) -> pd.DataFrame:
     Return asset values for 'past_period' year til 'year'.
     """
     asset_data: pd.DataFrame
-    asset_data = yf.Ticker(name).history(period="max")[["Open"]]
+    asset_data = yf.Ticker(INDEX_NAME[name]).history(period="max")[["Open"]]
     asset_data = asset_data[str(year - past_period): str(year-1)]
     asset_data = asset_data.rename(columns={'Open': 'asset_price'})
     return asset_data
@@ -56,75 +56,9 @@ def get_asset_param(name: str, year: int, past_period: int) -> (float):
         return (-1, -1)
 
 
-def get_SPX_data(year: int, past_period: int) -> pd.DataFrame:
-    """
-    Return asset values for 'past_period' year til 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["SPX"], year, past_period)
-
-
-def get_SPX_param(year: int, past_period: int) -> (float):
-    """
-    Return SPX param calculated on 'past_period' years before 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["SPX"], year, past_period)
-
-
-def get_DJX_data(year: int, past_period: int) -> pd.DataFrame:
-    """
-    Return asset values for 'past_period' year til 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["DJX"], year, past_period)
-
-
-def get_DJX_param(year: int, past_period: int) -> (float):
-    """
-    Return SPX param calculated on 'past_period' years before 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["DJX"], year, past_period)
-
-
-def get_NDX_data(year: int, past_period: int) -> pd.DataFrame:
-    """
-    Return asset values for 'past_period' year til 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["NDX"], year, past_period)
-
-
-def get_NDX_param(year: int, past_period: int) -> (float):
-    """
-    Return SPX param calculated on 'past_period' years before 'year' for SPX.
-    """
-    return get_asset_data(INDEX_NAME["NDX"], year, past_period)
-
-
 def display_asset(name: str, year: int, past_period: int) -> None:
     """
     Show the asset prices between year-past_period and year.
     """
     get_asset_data(name, year, past_period)["asset_price"].plot()
-    plt.show()
-
-
-def display_SPX(year: int, past_period: int) -> None:
-    """
-    Show SPX between year-past_period and year.
-    """
-    get_SPX_data(year, past_period)["asset_price"].plot()
-    plt.show()
-
-
-def display_DJX(year: int, past_period: int) -> None:
-    """
-    Show SPX between year-past_period and year.
-    """
-    get_SPX_data(year, past_period)["asset_price"].plot()
-    plt.show()
-
-
-def display_NDX(year: int, past_period: int) -> None:
-    """
-    Show SPX between year-past_period and year.
-    """
-    get_SPX_data(year, past_period)["asset_price"].plot()
     plt.show()
