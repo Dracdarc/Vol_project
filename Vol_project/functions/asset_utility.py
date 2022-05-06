e251c57861eb3ec70bba765a3e78b4a7a5610b1d
Dracdarc <rpecheul@yahoo.fr>
2022-05-06 17:37:15 +0200

    param calculation

--- Vol_project/functions/asset_utility.py
+++ Vol_project/functions/asset_utility.py
@@ -59,15 +59,13 @@ def get_asset_param(name: str, year: int, past_period: int) -> (float):
 def get_realized_volatility_list(
     name: str, date_series: "pd.series", days_retro: int = 25
 ) -> [float]:
-    print(0)
     asset_data: pd.DataFrame = get_asset_data(
-        name, date_series.iloc[-1].years + 1, past_period=2
+        name, date_series.iloc[-1].year + 1, past_period=2
     )
     realized_volatility: [float] = []
     index: int = asset_data.index[
         asset_data["date"] == date_series.iloc[0]
     ].to_list()[0]
-    print(1)
     local_info: pd.DataFrame
     for i in range(len(date_series)):
         local_info = asset_data[(index-days_retro):index]
